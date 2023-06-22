d67b6015ac9f87b58e441893e3191899a0580e22
tiago <tiagoandroloko@gmail.com>
2023-06-22 14:33:18 -0300

    bg alterado

--- src/assets/components/page/Serie.jsx
+++ src/assets/components/page/Serie.jsx
@@ -17,7 +17,7 @@ const Serie = () => {
   return (
     <div id="home" className="container">
       <h1 className="home-title">
-        TOP <span>SERIES</span> DA SEMANA{" "}
+        <span>SERIES</span> MAIS BEM AVALIADAS{" "}
       </h1>
 
       {error && <p>{error}</p>}
