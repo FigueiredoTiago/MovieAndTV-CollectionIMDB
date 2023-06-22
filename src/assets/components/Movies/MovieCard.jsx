659fdbc13330526b60b442a043e9ae8267d91e25
tiago <tiagoandroloko@gmail.com>
2023-06-22 16:02:14 -0300

    alguns efeitoss

--- src/assets/components/Movies/MovieCard.jsx
+++ src/assets/components/Movies/MovieCard.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import { Link } from "react-router-dom";
-import imdb from "../../sass/img/imdb.svg"; 
-import notFound from '../../sass/img/imgnotfound.jpg'; 
+import imdb from "../../sass/img/imdb.svg";
+import notFound from "../../sass/img/imgnotfound.jpg";
 
 const MovieCard = ({ movie, showLink = true }) => {
   const imgURL = import.meta.env.VITE_IMG;
 
   return (
-    <div className="grid-3 movie-card">
+    <div className="grid-3 movie-card animateIn">
       {movie.poster_path === null && (
         <img
           className="poster-img-not-found"
@@ -76,19 +76,19 @@ const MovieCard = ({ movie, showLink = true }) => {
       {movie.media_type == "person" && <h1 className="title-card">Actor</h1>}
 
       {showLink && movie.media_type == "movie" && (
-        <Link className="more" to={`/movie/${movie.id}`}>
+        <Link className="btn" to={`/movie/${movie.id}`}>
           Ver Mais..
         </Link>
       )}
 
       {showLink && movie.media_type == "tv" && (
-        <div className="more">
+        <div className="btn">
           <Link to={`/tv/${movie.id}`}>Ver Mais...</Link>
         </div>
       )}
 
       {showLink && movie.media_type == "person" && (
-        <div className="more">
+        <div className="btn">
           <Link to={`/person/${movie.id}`}>Ver Mais...</Link>
         </div>
       )}
