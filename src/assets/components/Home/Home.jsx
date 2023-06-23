d73bf9c46ba0d3ab82f4254dd21983b97980582e
tiago <tiagoandroloko@gmail.com>
2023-06-23 09:03:11 -0300

    animation h1

--- src/assets/components/Home/Home.jsx
+++ src/assets/components/Home/Home.jsx
@@ -2,6 +2,7 @@
 import useGet from "../../Hooks/useGet";
 import Loading from "../Loading/Loading";
 import MovieCard from "../Movies/MovieCard";
+import { TypeAnimation } from "react-type-animation";
 
 const Home = () => {
   const apiTopMovies = import.meta.env.VITE_TOP_DAY;
@@ -11,13 +12,29 @@ const Home = () => {
     `${apiTopMovies}?${apikey}&language=pt-BR`
   );
 
-  console.log(data);
-
   if (isLoading) return <Loading />;
   return (
     <div id="home" className="container">
-      <h1 className="home-title">
-        TOP <span>FILMES</span> E <span>SERIES</span> DA SEMANA{" "}
+      <h1 className="title-animation">
+        <TypeAnimation
+          sequence={[
+            "Bem Vindo ao CineScore!",
+            2500,
+            "Navegue Pelo Catalogo do IMDB",
+            2000,
+            "E Descubra Novos Filmes e Series...",
+            2000,
+            "Abaixo Top Filmes e Series do Dia:",
+            9000,
+          ]}
+          wrapper="span"
+          speed={50}
+          style={{
+            display: "inline-block",
+            color: "#f2b84b",
+          }}
+          repeat={Infinity}
+        />
       </h1>
 
       {error && <p>{error}</p>}
